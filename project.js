ecd732685a7a82d368c7e3b7ecc2efcc3b8cc414
bugsymangesh23 <emmanuelmangera08@gmail.com>
2025-01-24 16:47:42 +0300

    feat: Display user with the spin combinations

--- project.js
+++ project.js
@@ -107,8 +107,27 @@ const transpose = (reels) => {
     return rows;
 };
 
-const reels = spin();
-console.log(reels);
+// Display to user the slot spin
+
+const printRows= (rows) => {
+    for (const row of rows) {
+        let rowString = " ";
+        for (const [i, symbol] of row.entries()) {
+            rowString += symbol;
+            if (i != row.length - 1) { 
+                rowString += " | "
+            }
+        }
+        console.log(rowString);
+    }
+};
+
+
+
 let balance = deposit();
 const numberOfLines = getNumberOfLines();
 const bet = getBet(balance, numberOfLines);
+const reels = spin();
+const rows = transpose(reels);
+console.log(rows);
+printRows(rows);  
